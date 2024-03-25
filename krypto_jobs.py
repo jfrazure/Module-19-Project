3beb1dfe32851ac855e7f39b756f0fc546d8baa0
jfrazure <jfrazure@gmail.com>
2024-03-25 10:48:23 -0500

    web3 to w3 changes

--- krypto_jobs.py
+++ krypto_jobs.py
@@ -30,7 +30,7 @@ from dataclasses import dataclass
 from typing import Any, List
 from web3 import Web3
 
-web3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 ################################################################################
 # Step 1:
 # Import Ethereum Transaction Functions into the KryptoJobs2Go Application
@@ -299,7 +299,7 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    transaction_hash = send_transaction(web3, account, candidate_address, wage)
+    transaction_hash = send_transaction(w3, account, candidate_address, wage)
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
